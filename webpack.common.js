a1bb075ed93fc650633fd82959fe0576e99f99a0
jinho-choi123 <choijinho817@gmail.com>
2023-12-20 01:16:16 +0900

    config: add webpack loaders to process react jsx, css, sass

--- webpack.common.js
+++ webpack.common.js
@@ -23,5 +23,43 @@ module.exports = {
                 {from: 'manifest.json', to: ''},
             ]
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+            test: /\.jsx?/,
+            exclude: /node_modules/,
+            use: {
+            loader: 'babel-loader',
+            options: {
+                presets: [
+                '@babel/preset-env', 
+                '@babel/preset-react'
+                ]
+            }
+        }
+        },
+        {
+            test: /\.s[ac]ss$/i,
+            use: [
+                // Creates `style` nodes from JS strings
+                "style-loader",
+                // Translates CSS into CommonJS
+                "css-loader",
+                // Compiles Sass to CSS
+                "sass-loader",
+            ],
+        },
+        {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+            
+        },
+        ]
+    },
+    resolve: {
+        alias: {
+            src: path.resolve(__dirname, 'src/'),
+        }
+    }
 }
