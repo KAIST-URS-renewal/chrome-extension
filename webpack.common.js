fba754801d637ca929ef049cca00bfa8aaae082b
jinho-choi123 <choijinho817@gmail.com>
2023-12-31 19:36:13 +0900

    misc: format using prettier

--- webpack.common.js
+++ webpack.common.js
@@ -2,60 +2,60 @@ const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: {
-        background: {
-            import: './background.js',
-            filename: 'background.js',
-        },
-        content_script: {
-            import: './content_script.js',
-            filename: 'content_script.js',
-        },
-        popup: {
-            import: './popup.js',
-            filename: 'popup.js',
-        },
+  entry: {
+    background: {
+      import: './background.js',
+      filename: 'background.js',
     },
-    plugins: [
-        new CopyPlugin({
-            patterns: [
-                { from: 'popup.html', to: '' },
-                { from: 'manifest.json', to: '' },
-            ],
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.jsx?/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react'],
-                    },
-                },
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    'style-loader',
-                    // Translates CSS into CommonJS
-                    'css-loader',
-                    // Compiles Sass to CSS
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
+    content_script: {
+      import: './content_script.js',
+      filename: 'content_script.js',
+    },
+    popup: {
+      import: './popup.js',
+      filename: 'popup.js',
+    },
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {from: 'popup.html', to: ''},
+        {from: 'manifest.json', to: ''},
+      ],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
         ],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, 'src/'),
     },
-    resolve: {
-        alias: {
-            src: path.resolve(__dirname, 'src/'),
-        },
-    },
+  },
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,12 +1,12 @@
-const { merge } = require('webpack-merge');
+const {merge} = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    watchOptions: {
-        aggregateTimeout: 200,
-        poll: 1000,
-    },
-    watch: true,
+  mode: 'development',
+  devtool: 'inline-source-map',
+  watchOptions: {
+    aggregateTimeout: 200,
+    poll: 1000,
+  },
+  watch: true,
 });
