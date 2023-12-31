cd6cddabf7b6f136da1489f228c561f41315f44e
jinho-choi123 <choijinho817@gmail.com>
2023-12-31 19:39:24 +0900

    feat: get user name, cell #, email

--- src/data/urs/getUserInfo.js
+++ src/data/urs/getUserInfo.js
@@ -2,52 +2,52 @@ const superagent = require('superagent');
 var HTMLParser = require('node-html-parser');
 var _ = require('lodash');
 
-const getUserInfoHtml = async (facilityId) => {
-    try {
-        const res = await superagent
-            .post('/urs/rsv/app/cmn/RsvAppCmn001M02.do')
-            .accept('application/json')
-            .field('prgrId', facilityId);
+const getUserInfoHtml = async facilityId => {
+  try {
+    const res = await superagent
+      .post('/urs/rsv/app/cmn/RsvAppCmn001M02.do')
+      .accept('application/json')
+      .field('prgrId', facilityId);
 
-        return res.text;
-    } catch (err) {
-        console.error(err);
-        return null;
-    }
+    return res.text;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
 };
 
-const parseUserInfoHtml = (res) => {
-    if (!res) {
-        console.error('Invalid input for parse UserInfo Html function.');
-        return null;
-    }
+const parseUserInfoHtml = res => {
+  if (!res) {
+    console.error('Invalid input for parse UserInfo Html function.');
+    return null;
+  }
 
-    const root = HTMLParser.parse(res).removeWhitespace();
+  const root = HTMLParser.parse(res).removeWhitespace();
 
-    const userName = root.querySelector('#rsvtAppEmplNm').getAttribute('value');
-    const userCell = root.querySelector('#rsvtAppHpNo').getAttribute('value');
-    const userEmail = root
-        .querySelector('#rsvtAppEmailAddr')
-        .getAttribute('value');
+  const userName = root.querySelector('#rsvtAppEmplNm').getAttribute('value');
+  const userCell = root.querySelector('#rsvtAppHpNo').getAttribute('value');
+  const userEmail = root
+    .querySelector('#rsvtAppEmailAddr')
+    .getAttribute('value');
 
-    var json = new Object();
+  var json = new Object();
 
-    json.userName = userName;
-    json.userCell = userCell;
-    json.userEmail = userEmail;
+  json.userName = userName;
+  json.userCell = userCell;
+  json.userEmail = userEmail;
 
-    return json;
+  return json;
 };
 
 export const getUserInfo = async () => {
-    const start = Date.now();
-    const html = await getUserInfoHtml(0);
+  //const start = Date.now();
+  const html = await getUserInfoHtml(0);
 
-    const userInfo = parseUserInfoHtml(html);
-    console.log(userInfo);
+  const userInfo = parseUserInfoHtml(html);
+  //console.log(userInfo);
 
-    const end = Date.now();
-    console.log(`Execution time of getFacility: ${end - start} ms`);
+  //const end = Date.now();
+  //console.log(`Execution time of getFacility: ${end - start} ms`);
 
-    return userInfo;
+  return userInfo;
 };
